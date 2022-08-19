fd61798b4f6392fa04c4ababbb259bbb08e70957
“rotomoo” <quddlf410@naver.com>
2022-08-19 18:58:26 +0900

    #1 - feat : project

---
+++ src/main/java/me/bi/oembedapi/OembedapiApplication.java
@@ -0,0 +1,13 @@
+package me.bi.oembedapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OembedapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OembedapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/bi/oembedapi/OembedapiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.bi.oembedapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OembedapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
